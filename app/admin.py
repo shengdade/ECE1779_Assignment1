4808c185e4494f4ae7e28bf8bf29ae50f6c80357
Dade Sheng <dade@live.cn>
2017-03-03 17:41:16 -0500

    run cloud-config when creating intances

--- app/admin.py
+++ app/admin.py
@@ -6,22 +6,29 @@ from flask import redirect, url_for, request
 import config
 from app import webapp
 
+# ARN = 'arn:aws:elasticloadbalancing:us-east-1:554376045366:targetgroup/a1-worker-group/e254bac50246fab2'
+
 
 @webapp.route('/admin/create', methods=['POST'])
 def ec2_create():
     num_create = int(request.form.get('num-new'))
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.resource('ec2', **config.conn_args)
+    elb = boto3.client('elbv2')
     instances = ec2.create_instances(ImageId=config.ami_id,
                                      InstanceType=config.instance_type,
                                      SecurityGroups=config.security_group,
                                      KeyName=config.key_name,
+                                     UserData=config.userdata,
                                      MinCount=1,
                                      MaxCount=num_create)
     id_list = []
+    # id_port_list = []
     for ins in instances:
         id_list.append(ins.id)
+        # id_port_list.append({'Id': ins.id, 'Port': 80})
 
     ec2.create_tags(Resources=id_list, Tags=[{'Key': 'Name', 'Value': 'a1-worker'}])
+    # elb.register_targets(TargetGroupArn=ARN, Targets=id_port_list)
 
     return redirect(url_for('admin'))
 
--- app/config.py
+++ app/config.py
@@ -7,3 +7,23 @@ ami_id = 'ami-e3f432f5'
 instance_type = 't2.small'
 security_group = ['a1-worker']
 key_name = 'ece1779'
+
+conn_args = {
+    'aws_access_key_id': 'AKIAJ3J2T2OKN4H6HDCA',
+    'aws_secret_access_key': '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',
+    'region_name': 'us-east-1'
+}
+
+# define userdata to be run at instance launch
+userdata = """#cloud-config
+
+runcmd:
+ - cd
+ - git clone https://github.com/shengdade/ECE1779_Assignment1.git
+ - cd ECE1779_Assignment1
+ - yes | pip install gunicorn
+ - yes | pip install flask
+ - yes | pip install boto3
+ - mysql --user=ece1779 --password=secret < ece1779_a1.sql
+ - ./run.sh
+"""
