4e66619fa2c445fdd35925effb14a5abfa948f81
Dade Sheng <dade@live.cn>
2017-03-06 12:05:56 -0500

    update config

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 db_config = {'user': 'ece1779',
              'password': 'secret',
-             'host': '0.0.0.0',
+             'host': '54.209.253.37',
              'database': 'ece1779_a1'}
 
 ami_id = 'ami-e3f432f5'
@@ -9,24 +9,24 @@ security_group = ['a1-worker']
 key_name = 'ece1779'
 
 # for assignment 1
-# ARN = 'arn:aws:elasticloadbalancing:us-east-1:554376045366:targetgroup/a1-worker-group/e254bac50246fab2'
+ARN = 'arn:aws:elasticloadbalancing:us-east-1:554376045366:targetgroup/a1-worker-group/e254bac50246fab2'
 
 # for dade ec2
-ARN = 'arn:aws:elasticloadbalancing:us-east-1:415416742824:targetgroup/a1-worker-group/a9217aa5e0cdc06b'
+# ARN = 'arn:aws:elasticloadbalancing:us-east-1:415416742824:targetgroup/a1-worker-group/a9217aa5e0cdc06b'
 
 # for assignment 1
-# conn_args = {
-#     'aws_access_key_id': 'AKIAI7RZC7WJCND5SESA',
-#     'aws_secret_access_key': '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',
-#     'region_name': 'us-east-1'
-# }
-
-# for dade ec2
 conn_args = {
-    'aws_access_key_id': 'AKIAIL4GGZP6HRGUBRUA',
-    'aws_secret_access_key': '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',
+    'aws_access_key_id': 'AKIAI7RZC7WJCND5SESA',
+    'aws_secret_access_key': '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',
     'region_name': 'us-east-1'
 }
+
+# for dade ec2
+# conn_args = {
+#     'aws_access_key_id': 'AKIAIL4GGZP6HRGUBRUA',
+#     'aws_secret_access_key': '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',
+#     'region_name': 'us-east-1'
+# }
 
 # define userdata to be run at instance launch
 userdata = """#cloud-config
@@ -42,12 +42,14 @@ runcmd:
  - yes | pip install flask
  - yes | pip install boto3
  - yes | pip install celery
- - ./install_redis.sh
- - redis-stable/src/redis-server --daemonize yes
- - pip install redis
+ - yes | pip install redis
+ - apt install redis-server
+ - redis-server --daemonize yes
  - celery worker -A app -l info
  - mysql --user=ece1779 --password=secret < ece1779_a1.sql
  - ./run.sh
 """
 
+# - ./install_redis.sh
+# - redis-stable/src/redis-server --daemonize yes
 # /var/log/cloud-init-output.log
