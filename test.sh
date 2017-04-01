56402f16428df56bf2c48aaa0c1ee4d5960bbc11
Dade Sheng <dade@live.cn>
2017-04-01 03:07:30 -0400

    update test and options

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 cd /Users/dadesheng/Workspace/ECE1779_Assignment1/ece1779LoadGenerator/bin
-java -cp . ece1779.loadgenerator.LoadGenerator 50.17.36.200 80 dddaaa dddaaa &
+java -cp . ece1779.loadgenerator.LoadGenerator 54.174.193.192 80 dddaaa2 dddaaa2 &
